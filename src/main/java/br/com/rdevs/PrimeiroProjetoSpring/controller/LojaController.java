c483559ae584759aa4ff7078752c2654e6164843
João Claudio <joaoclaudiodiva@gmail.com>
2020-08-12 12:40:06 -0300

    Ajustado metodo buscarPorNome

--- src/main/java/br/com/rdevs/PrimeiroProjetoSpring/controller/LojaController.java
+++ src/main/java/br/com/rdevs/PrimeiroProjetoSpring/controller/LojaController.java
@@ -1,10 +1,12 @@
 package br.com.rdevs.PrimeiroProjetoSpring.controller;
 
 import br.com.rdevs.PrimeiroProjetoSpring.model.dto.LojaDTO;
+import br.com.rdevs.PrimeiroProjetoSpring.model.dto.ResultData;
 import br.com.rdevs.PrimeiroProjetoSpring.model.entity.LojaEntity;
 import br.com.rdevs.PrimeiroProjetoSpring.repository.LojaRepository;
 import br.com.rdevs.PrimeiroProjetoSpring.service.LojaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,15 +31,34 @@ public class LojaController {
         return ResponseEntity.ok().body(service.buscar(codigo));
     }
 
-    @GetMapping("/loja/buscarPorNome/{nome}")
-    public ResponseEntity burcarPorNome (@PathParam("nome") String nome){
+    @GetMapping("/loja/buscarPorNome")
+    public ResponseEntity burcarPorNome (@RequestParam("nome") String nome){
+        System.out.println(nome);
         return ResponseEntity.ok().body(service.buscarPorNome(nome));
     }
 
     @PostMapping("/loja")
-    public ResponseEntity inserir(@RequestBody LojaDTO lojaDTO){
-        service.inserir(lojaDTO);
-        return  ResponseEntity.ok().body(lojaDTO);
+    public ResponseEntity<Object> inserir(@RequestBody LojaDTO lojaDTO){
+        ResultData result = null;
+        if(lojaDTO.getNome() == null) {
+             result = new ResultData(HttpStatus.BAD_REQUEST.value(),"Atributo Nome Invalido");
+        }
+        else if(lojaDTO.getLogomarca() == null){
+            result = new ResultData(HttpStatus.BAD_REQUEST.value(),"Atributo Logomarca Invalido");
+        }
+        if(result == null) {
+            return ResponseEntity.badRequest().body(result);
+        }
+
+        try{
+            service.inserir(lojaDTO);
+            result = new ResultData(HttpStatus.CREATED.value(),"Dados de Loja inseridos com Sucesso!",lojaDTO);
+            return  ResponseEntity.status(HttpStatus.CREATED).body(result);
+        }catch (Exception e){
+            result = new ResultData(HttpStatus.BAD_REQUEST.value(),"Erro ao criar loja " + e.getMessage());
+            return  ResponseEntity.badRequest().body(result);
+        }
+
     }
 
     @PutMapping("/loja")
@@ -46,7 +67,7 @@ public class LojaController {
         return  ResponseEntity.ok().body(lojaDTO);
     }
 
-    @DeleteMapping("/loja")
+    @DeleteMapping("/loja/{codigo}")
     public  ResponseEntity deletar(@PathVariable("codigo") Integer codigo){
         return ResponseEntity.ok().body(service.excluir(codigo));
     }
